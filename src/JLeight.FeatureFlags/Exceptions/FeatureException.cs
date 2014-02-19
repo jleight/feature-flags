3d001f2ce891d569b0515695f4dc7480c03b0b98
Jonathon Leight <jonathon.leight@jleight.com>
2014-02-19 18:52:53 -0500

    Implementing base feature exception.

---
+++ src/JLeight.FeatureFlags/Exceptions/FeatureException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace JLeight.FeatureFlags.Exceptions
+{
+    [Serializable]
+    public class FeatureException
+        : Exception
+    {
+        public FeatureException()
+        {
+        }
+
+        public FeatureException(string message)
+            : base(message)
+        {
+        }
+
+        public FeatureException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected FeatureException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
