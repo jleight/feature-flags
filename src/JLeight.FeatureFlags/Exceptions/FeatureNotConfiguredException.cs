4bf2c6309548ddd9b8756d47ebc30f4c8138bd2e
Jonathon Leight <jonathon.leight@jleight.com>
2014-02-19 19:02:29 -0500

    Implementing feature not configured exception.
    When all configuration options have been exhausted, this exception will
    be thrown.

---
+++ src/JLeight.FeatureFlags/Exceptions/FeatureNotConfiguredException.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace JLeight.FeatureFlags.Exceptions
+{
+    [Serializable]
+    public class FeatureNotConfiguredException
+        : FeatureException
+    {
+        public string FeatureName { get; private set; }
+
+        public override string Message
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(this.FeatureName))
+                    return base.Message;
+                return base.Message +
+                    Environment.NewLine +
+                    "Feature name: " +
+                    this.FeatureName;
+            }
+        }
+
+
+        public FeatureNotConfiguredException()
+            : base("Feature configuration could not be found.")
+        {
+        }
+
+        public FeatureNotConfiguredException(string name)
+            : this()
+        {
+            this.FeatureName = name;
+        }
+
+        public FeatureNotConfiguredException(string name, string message)
+            : base(message)
+        {
+            this.FeatureName = name;
+        }
+
+        public FeatureNotConfiguredException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected FeatureNotConfiguredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
