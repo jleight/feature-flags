2533eec38db3a96cf66514e4a0d40b521f814af7
Jonathon Leight <jonathon.leight@jleight.com>
2014-02-19 19:10:16 -0500

    Adding feature factory class with attribute method.
    FeatureFactory will be used internally to create instances of features
    from various high level sources.

---
+++ src/JLeight.FeatureFlags.Tests/FeatureFactoryFacts.cs
@@ -0,0 +1,23 @@
+﻿using JLeight.FeatureFlags.Attributes;
+using JLeight.FeatureFlags.Factories;
+using Xunit;
+
+namespace JLeight.FeatureFlags.Tests
+{
+    public class FeatureFactoryFacts
+    {
+        [Fact]
+        public void EnabledAttributeFeatureIsEnabled()
+        {
+            var feature = FeatureFactory.From("Feature", new EnabledAttribute());
+            Assert.True(feature.IsEnabled);
+        }
+
+        [Fact]
+        public void DisabledAttributeFeatureIsDisabled()
+        {
+            var feature = FeatureFactory.From("Feature", new DisabledAttribute());
+            Assert.False(feature.IsEnabled);
+        }
+    }
+}
---
+++ src/JLeight.FeatureFlags/Factories/FeatureFactory.cs
@@ -0,0 +1,17 @@
+﻿using JLeight.FeatureFlags.Attributes;
+using JLeight.FeatureFlags.Exceptions;
+
+namespace JLeight.FeatureFlags.Factories
+{
+    internal static class FeatureFactory
+    {
+        public static Feature From(string name, IFeatureAttribute attribute)
+        {
+            if (attribute is EnabledAttribute)
+                return new Feature(name, true);
+            if (attribute is DisabledAttribute)
+                return new Feature(name, false);
+            throw new FeatureNotConfiguredException(name);
+        }
+    }
+}
