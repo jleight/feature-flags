098623baa674a89e0a3f1a012033693409bde79a
Jonathon Leight <jonathon.leight@jleight.com>
2014-02-19 19:34:52 -0500

    Adding test to make sure the App.config file works.

---
+++ src/JLeight.FeatureFlags.Tests/AppSettingsFacts.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using Xunit;
+namespace JLeight.FeatureFlags.Tests
+{
+    public class AppSettingsFacts
+    {
+        [Fact]
+        public void AppSettingsAreReadable()
+        {
+            var value = ConfigurationManager.AppSettings["TestValue"];
+            Assert.False(string.IsNullOrEmpty(value));
+        }
+    }
+}
